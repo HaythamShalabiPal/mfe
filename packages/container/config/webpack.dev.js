008fe1301b976646164acbb32fd406c9c1dd5b79
HaythamShalabiPal <h.y.k.a1995@gmail.com>
2022-12-30 21:22:46 +0200

    MFE

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -8,7 +8,7 @@ const config = {
     devServer: {
         port: 8080,
         historyApiFallback: {
-            index: 'index.html'
+            index: '/index.html'
         }
     },
     plugins: [
--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
-export default () => {
-    return <MarketingApp />
-};
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
+
+export default () => (
+    <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+            <Header />
+            <MarketingApp />
+        </BrowserRouter>
+    </StylesProvider>
+)
